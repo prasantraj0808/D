cf3da2da9cc277864cf95d2da464dce823c9d30d
Prasant <prasantraj0808@gmail.com>
2022-05-13 21:40:27 +0530

    find string in grid || runtime error

--- Desktop/doubt/D.cpp
+++ Desktop/doubt/D.cpp
@@ -1,53 +1,76 @@
-
-
-#include <bits/stdc++.h>
+// { Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+ // } Driver Code Ends
+class Solution {
 public:
-
-    int wordBreak(string s, vector<string> &B) {
-       
-        set<string>Set;
-        for(auto x:B)
-        {
-            Set.insert(x);
-        }
-        bool dp[s.length()+1];
-        dp[0]=true;
-        for(int len=1;len<=s.length();len++)
-        {
-            for(int i=0;i<len;i++)
-            {
-                if(dp[i] && Set.find(s.substr(i,len))!=Set.end())
-                {
-                  dp[len]=true;
-                  break;
-                }
-            }
-        }
-        return dp[s.length()];
-    }
+	vector<vector<int>>searchWord(vector<vector<char>>grid, string word){
+	    // Code here
+	    int n=grid.size(),m=grid[0].size(),len=word.length(); //grid rows// grid columns//string length
+	    int di[]={0,1,1,1,0,-1,-1,-1};
+	    int dj[]={1,1,0,-1,-1,-1,0,1};
+	    int pos=1,flag=0,anspos=0;
+	    vector<vector<int>> ans;
+	    for(int i=0;i<m;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        {
+	          
+	            if(word[0]==grid[i][j])
+	            {
+	                for(int k=0;k<8;k++)
+	                {
+	                     for(int p=1;p<len;p++)
+	                     {
+	                             if(word[p]!=grid[i+di[k]][j+dj[k]])
+	                             {
+	                                 flag=1;
+	                                 break;
+	                             }
+	                     }
+	                     if(flag==0)
+	                     {
+	                         ans[anspos].push_back(i);
+	                         ans[anspos++].push_back(j);
+	                         break;
+	                     }
+	                     
+	                }
+	                flag=0;
+	            }
+	        }
+	    }
+	    
+	    return ans;
+	}
 };
 
-
-
+// { Driver Code Starts.
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<string> dict;
-        for(int i=0;i<n;i++){
-            string S;
-            cin>>S;
-            dict.push_back(S);
-        }
-        string line;
-        cin>>line;
-        Solution ob;
-        cout<<ob.wordBreak(line, dict)<<"\n";
-    }
-}
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<char>>grid(n, vector<char>(m,'x'));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++)
+				cin >> grid[i][j];
+		}
+		string word;
+		cin >> word;
+		Solution obj;
+		vector<vector<int>>ans = obj.searchWord(grid, word);
+		for(auto i: ans){
+			for(auto j: i)
+				cout << j << " ";
+			cout << "\n";
+		}
+		if(ans.size() == 0)
+		{
+		    cout<<"-1\n";
+		}
+	}
+	return 0;
+}  // } Driver Code Ends
